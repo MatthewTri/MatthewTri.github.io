bf7eba3b243ff07e931078974ad0874a63e3e3c8
MatthewTri <C14220080@john.petra.ac.id>
2025-03-23 17:01:32 +0700

    tambah

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,8 @@
 var title = document.querySelector('.title');
 var courseFeatureElements = document.querySelectorAll('.course-feature');
 var button = document.querySelector('button');
+var installButton = document.getElementById('install-button');
+let deferredPrompt;
 
 navigator.serviceWorker.register('/sw.js');
 
@@ -52,4 +54,34 @@ animate();
 
 button.addEventListener('click', function() {
   animate();
+});
+
+// Install button logic
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent the mini-infobar from appearing on mobile
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI notify the user they can install the PWA
+  installButton.style.display = 'block';
+
+  installButton.addEventListener('click', (e) => {
+    // Hide the install button
+    installButton.style.display = 'none';
+    // Show the install prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the install prompt');
+      } else {
+        console.log('User dismissed the install prompt');
+      }
+      deferredPrompt = null;
+    });
+  });
+});
+
+window.addEventListener('appinstalled', (evt) => {
+  console.log('PWA was installed');
 });
