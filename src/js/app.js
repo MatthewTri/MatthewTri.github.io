7ab85727b3542e2b5c31832c26a917bec565cf3f
MatthewTri <C14220080@john.petra.ac.id>
2025-03-24 08:57:38 +0700

    Penambahan install button

--- src/js/app.js
+++ src/js/app.js
@@ -20,5 +20,21 @@ window.addEventListener('beforeinstallprompt', function(event) {
   console.log('beforeinstallprompt fired');
   event.preventDefault();
   deferredPrompt = event;
+  document.getElementById('install-button').style.display = 'block';
   return false;
+});
+
+document.getElementById('install-button').addEventListener('click', function() {
+  document.getElementById('install-button').style.display = 'none';
+  if (deferredPrompt) {
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.then(function(choiceResult) {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the install prompt');
+      } else {
+        console.log('User dismissed the install prompt');
+      }
+      deferredPrompt = null;
+    });
+  }
 });
